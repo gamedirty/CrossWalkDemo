c1cfb3081295fb0b2350b14fb562c059112ca602
zhaojunhui <gamedirty@hotmail.com>
2016-08-15 19:25:09 +0800

    dd

---
+++ app/src/main/java/com/example/sovnem/crosswalkdemo/HullActivity.java
@@ -0,0 +1,16 @@
+package com.example.sovnem.crosswalkdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 外壳activity
+ */
+public class HullActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hull);
+    }
+}
